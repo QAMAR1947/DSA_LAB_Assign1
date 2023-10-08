9b8217ae6c3e02c39aa7f432fa6d51f308f4f169
Qamar <147144716+QAMAR1947@users.noreply.github.com>
2023-10-08 06:51:43 -0700

    Add files via upload
    code to add even and odd elements of list in separate lists and delete nodes from main list

---
+++ question 9.cpp
@@ -0,0 +1,115 @@
+#include<iostream>
+using namespace std;
+
+// CREATING STRUCTURE OF LINKED LIST
+struct NODE{
+	int data;
+	NODE* next;
+};
+
+// FUNCTION TO INSERT NODE AT THE END OF LIST
+void insert_end(struct NODE* &head, int x){	
+	NODE* new_node = new NODE();
+	new_node -> data = x;
+	new_node -> next = NULL; 
+	
+		// IF THERE IS NO NODE IN LIST (CORNER CASE)
+		if(head == NULL){
+		head = new_node;
+		return;
+	}
+	
+	NODE *current = head;
+	
+	while(current -> next != NULL){
+		current = current->next;
+	}
+	current->next = new_node;
+	
+}
+
+// FUNCTION TO PRINT NODES
+void print(struct NODE* &head){
+	
+	if(head == NULL){
+		cout<<"LIST IS EMPTY"<<endl;
+		return;
+	}
+	
+	NODE* current = head;
+
+	while (current != NULL){
+		cout<<current->data<<", ";
+		current = current->next;
+	}
+	cout<<endl;
+	
+}
+
+// FUNCTION TO STORE ODD AND EVEN ELEMENTS IN SEPARATE LISTS
+void separate(struct NODE* &head, struct NODE* &list1, struct NODE* &list2){
+	NODE* temp1 = head;
+	NODE* temp2 = head;
+	
+	// if no node exist
+	if(head == NULL){
+		cout<<"LIST IS EMPTY"<<endl;
+		return;
+	}
+	
+	// if only 1 node exist
+	if(head->next == NULL){
+		cout<<"ONLY 1 NODE"<<endl;
+		return;
+	}
+	
+	
+	while(temp1 != NULL){
+		// for even value of node
+		if(temp1->data %2 == 0){
+			insert_end(list1, temp1->data);
+		}
+		
+		// for odd value of node
+		if(temp1->data %2 == 1){
+			insert_end(list2, temp1->data);
+		}
+		
+		// updating addresses
+		temp1 = temp1->next;
+		head = temp1;
+		delete(temp2);
+		temp2 = temp1;
+			
+	}
+}
+
+// MAIN FUNCTION
+int main(){
+	NODE* head1 = NULL;
+	NODE* list1 = NULL;
+	NODE* list2 = NULL;
+	
+	insert_end(head1,1);
+	insert_end(head1,2);
+	insert_end(head1,3);
+	insert_end(head1,4);
+	insert_end(head1,5);
+	
+	cout<<"PRINT BEFORE SEPARATE FUNCTION"<<endl;
+	print(head1);
+	
+	// calling separate function
+	separate(head1,list1,list2);
+	
+	cout<<"\nPRINT MAIN LIST AFTER SEPARATE FUNCTION"<<endl;
+	print(head1);
+	
+	cout<<"\nPRINT LIST 1 (EVEN) AFTER SEPARATE FUNCTION"<<endl;
+	print(list1);
+	cout<<"\nPRINT LIST 2 (ODD) AFTER SEPARATE FUNCTION"<<endl;
+	print(list2);
+	
+	
+	return 0;
+}
